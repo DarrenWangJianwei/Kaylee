685e154d5464462a564b01c12fdd0926c48796e8
wang Jianwei <darren.wjy1990@gmail.com>
2022-01-20 19:32:40 -0800

    project page with responsive css

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -3,40 +3,40 @@ import NavComponent from '../components/NavComponent';
 import MarkerComponent from '../components/MarkerComponent';
 import project from '../css/Project.module.css'
 import imageMap from '../images/hero_map.png';
+import imageLogo from '../images/logo_1.png';
 
 const Project = () => {
     return ( 
         <div className={project.container}>
-            <div className={project.logo}>
+            <div className={project.navContainer}>
+                <div className={project.logo}>
+                    <img src={imageLogo} alt="TK Red" width='60%'></img>
+                </div>
+                <div className={project.nav}>
+                    <NavComponent/>
+                </div>
+            </div>
 
-            </div>
-            <div className={project.nav}>
-                <NavComponent/>
-            </div>
             <div className={project.map}>
-                <img src={imageMap} alt="map of vancvouer" width="1100px" useMap="#map"/>
-                {/* 120px 190px */}
-                <MarkerComponent position={{top:'130px',left:'210px'}}/>
+                {/*width:1100px ---- width:300px*/}
+                <img src={imageMap} alt="map of vancvouer" useMap="#map"/>
+                {/* 120px 190px ---- 25px 63px*/}
+                <MarkerComponent position={{top:'14%',left:'20%'}}/>
                 {/* 415px 190px */}
-                <MarkerComponent position={{top:'455px',left:'205px'}}/>
+                <MarkerComponent position={{top:'46.5%',left:'20%'}}/>
                 {/* 400px 240px */}
-                <MarkerComponent position={{top:'440px',left:'265px'}}/>
+                <MarkerComponent position={{top:'45%',left:'25%'}}/>
                 {/* 360px 565px */}
-                <MarkerComponent position={{top:'400px',left:'630px'}}/>
+                <MarkerComponent position={{top:'40.5%',left:'58%'}} detailShowLeft={true}/>
                 {/* 570px 705px */}
-                <MarkerComponent position={{top:'630px',left:'780px'}}/>
+                <MarkerComponent position={{top:'63.5%',left:'71.5%'}} detailShowLeft={true}/>
                 {/* 625px 880px */}
-                <MarkerComponent position={{top:'690px',left:'970px'}} detailShowLeft={true}/>
+                <MarkerComponent position={{top:'69.5%',left:'89.5%'}} detailShowLeft={true}/>
                 {/* 435px 950px */}
-                <MarkerComponent position={{top:'480px',left:'1050px'}} detailShowLeft={true}/>
-                {/* <map name="map" >
-                    <area shape="rect" coords="34,44,270,350" alt="Computer" href="sss" />
-                    <area shape="rect" coords="290,172,333,250" alt="Phone" href="phone.htm" />
-                    <area shape="circle" coords="337,300,44" alt="Cup of coffee" href="coffee.htm"/>
-                </map> */}
+                <MarkerComponent position={{top:'48.5%',left:'96%'}} detailShowLeft={true}/>
+                <div className={project.subLogo}/>    
             </div>
 
-            <div className={project.subLogo}/>           
         </div>
     );
 }
